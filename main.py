c98dcad09331c63c6e31eec2ed62047c94ae46e8
Alexander <alexsandr.khramov@gmail.com>
2021-02-10 19:24:54 +0700

    (part 3) added arrows functionality

--- main.py
+++ main.py
@@ -30,6 +30,17 @@ class Main(QMainWindow, MainUi):
         self.image.loadFromData(QByteArray(response.content), "PNG")
         self.map_label.setPixmap(self.image)
 
+    def move_map(self, move_x, move_y):
+        z = int(self.static_map_params["z"])
+        dx = 360 / 2 ** z
+        dy = 180 / 2 ** z
+        self.static_map_x += dx * move_x
+        self.static_map_y += dy * move_y
+        self.static_map_x = (self.static_map_x + 180) % 360 - 180
+        self.static_map_y = min(self.static_map_y, 90)
+        self.static_map_y = max(self.static_map_y, -90)
+        self.static_map_params["ll"] = f'{self.static_map_x},{self.static_map_y}'
+
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_PageDown:
             z = int(self.static_map_params["z"])
@@ -42,6 +53,19 @@ class Main(QMainWindow, MainUi):
             if z + 1 <= 17:
                 z += 1
             self.static_map_params["z"] = str(z)
+
+        if event.key() == Qt.Key_A:
+            self.move_map(-1, 0)
+
+        if event.key() == Qt.Key_W:
+            self.move_map(0, 1)
+
+        if event.key() == Qt.Key_D:
+            self.move_map(1, 0)
+
+        if event.key() == Qt.Key_S:
+            self.move_map(0, -1)
+
         self.update_image()
 
 
