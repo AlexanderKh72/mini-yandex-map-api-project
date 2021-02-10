7f795323afe64cd65950ae860abc525cf91b5ad5
Alexander <alexsandr.khramov@gmail.com>
2021-02-10 17:28:25 +0700

    (part 1) added start code & edit ui file & converted .ui to .py

---
+++ main.py
@@ -0,0 +1,37 @@
+from PyQt5.QtWidgets import QMainWindow, QApplication
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import QByteArray
+
+from io import BytesIO
+import requests
+import sys
+from PIL import Image
+
+from map_app import Ui_MainWindow as MainUi
+
+
+class Main(QMainWindow, MainUi):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+        self.static_map_request = 'https://static-maps.yandex.ru/1.x/'
+        self.static_map_x = 37.617218
+        self.static_map_y = 55.751694
+        self.static_map_params = {"ll": f'{self.static_map_x},{self.static_map_y}',
+                                  "z": 10,
+                                  "size": '650,450',
+                                  "l": "map"}
+        self.update_image()
+
+    def update_image(self):
+        response = requests.get(self.static_map_request, params=self.static_map_params)
+        self.image = QPixmap()
+        self.image.loadFromData(QByteArray(response.content), "PNG")
+        self.map_label.setPixmap(self.image)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Main()
+    ex.show()
+    sys.exit(app.exec())
